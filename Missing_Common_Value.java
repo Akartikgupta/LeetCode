8262e0d9e7f4b2ea500090ee20b419d62b9145b1
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2023-11-04 12:11:54 +0530

    Missing Common Value Program
    Approach to do:
    1. Traverse the arrays as nums1 and nums2
    2. Find the common element in both the array's
    3. Compare with the last number of array 1 as nums1 or vice versa 
    4. Return the common value if any o/w return -1

---
+++ Missing_Common_Value.java
@@ -0,0 +1,21 @@
+public class Missing_Common_Value {
+    public static void main(String[] args) {
+        int nums1[] = { 1, 2 };
+        int nums2[] = { 2, 4 };
+        System.out.println(getCommon(nums1, nums2));
+    }
+
+    public static int getCommon(int[] nums1, int[] nums2) {
+        for (int i = 0; i <= nums1.length - 1; i++) {
+            for (int j = 0; j <= nums2.length - 1; j++) {
+                if (nums1[i] == nums2[j]) {
+                    int com = nums1[nums1.length - 1];
+                    if (nums1[i] <= com) {
+                        return nums1[i];
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
