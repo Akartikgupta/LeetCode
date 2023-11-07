5046bca23a7829a071b9931bb494e95f83a479a6
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2023-11-07 23:58:26 +0530

    Find Smallest Letter Greater Than Target Program
    Approach to solve this problem is:
    1. Apply the binary search normally
    2. Edit on when element is equal to target 
    3. Search for the left range and right range

---
+++ Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Find_Smallest_Letter_Greater_Than_Target {
+    public static void main(String[] args) {
+        int[] nums = { 5, 7, 7, 8, 8, 10 };
+        int target = 8;
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int result[] = { -1, -1 };
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                result[0] = mid; // Start index of the range
+
+                // Search left for the start of the range
+                while (result[0] > 0 && nums[result[0] - 1] == target) {
+                    result[0]--;
+                }
+
+                result[1] = mid; // End index of the range
+
+                // Search right for the end of the range
+                while (result[1] < nums.length - 1 && nums[result[1] + 1] == target) {
+                    result[1]++;
+                }
+
+                break; // Target found and range determined, exit the loop
+            }
+        }
+
+        return result;
+
+    }
+
+}
