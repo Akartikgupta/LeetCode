6dd21edc00cfdddcf93f2e3be47076ba8670db1f
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2023-11-08 19:18:31 +0530

    Maximum_Count_of_Positive_Integer_and_Negative_Integer
    Approach to solve this problem:-
    1. Iterate the array.
    2. Count the number's of positive number.
    3. Count the number's of negative number.

---
+++ Maximum_Count_of_PositiveInteger_and_Negative_Integer.java
@@ -0,0 +1,26 @@
+public class Maximum_Count_of_PositiveInteger_and_Negative_Integer {
+    public static void main(String[] args) {
+
+        int arr[] = { -3, -2, -1, 0, 0, 1, 2 };
+        System.out.println(maximumCount(arr));
+
+    }
+
+    public static int maximumCount(int[] nums) {
+        int pos = 0;
+        int neg = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                pos++;
+            } else if (nums[i] < 0) {
+                neg++;
+            }
+        }
+        if (pos > neg) {
+            return pos;
+        } else {
+            return neg;
+        }
+    }
+
+}
